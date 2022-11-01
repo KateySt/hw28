8705a4fc89450dfaa59c31e07810e70d8fd465ca
KateySt <kateystetsenko@gmail.com>
2022-11-01 22:07:50 +0200

    hw28

--- src/TextInput.tsx
+++ src/TextInput.tsx
@@ -13,31 +13,31 @@ function TextInput() {
     const [num, setNum] = useState<numI>({num: 0});
     let arr = new Array("");
 
-    const fillArr = (num: number, value: string): any => {
+    const fillArr = (num: number, value: string): void => {
         arr = [...Array(num)].map(() => value);
     };
 
-    const clickHandlerText = (stateSetter: Dispatch<textI>, e: any) => {
+    const clickHandlerText = (stateSetter: Dispatch<textI>, e: React.ChangeEvent<{ value: string }>) => {
         stateSetter({value: e.target.value});
     }
 
-    const clickHandlerNum = (stateSetter: Dispatch<numI>, e: any) => {
-        if(e.target.value==='')
+    const clickHandlerNum = (stateSetter: Dispatch<numI>, e: React.ChangeEvent<{ value: string }>) => {
+        if (e.target.value === '')
             stateSetter({num: 0});
         else
             stateSetter({num: parseInt(e.target.value)});
     }
 
+    fillArr(num.num, text.value);
+
     return (
         <div>
             <p>Input text: </p>
-            <input onChange={(e: React.FormEvent<HTMLInputElement>) => clickHandlerText(setText, e)}/>
+            <input onChange={(e: React.ChangeEvent<{ value: string }>) => clickHandlerText(setText, e)}/>
             <br/>
             <p>Input num: </p>
-            <input onChange={(e: React.FormEvent<HTMLInputElement>) => clickHandlerNum(setNum, e)}/>
+            <input onChange={(e: React.ChangeEvent<{ value: string }>) => clickHandlerNum(setNum, e)}/>
             <br/>
-            <br/>
-            <p>{fillArr(num.num, text.value)}</p>
             <div>
                 {arr.map((name: string, id: number): JSX.Element => {
                     return <p key={id}>{name}</p>;
